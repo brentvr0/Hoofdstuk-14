76855c4aadd88c6f608090b5ac21f93803ffeec4
Brent Van Rysselberghe <75610555+brentvr0@users.noreply.github.com>
2023-03-16 16:41:19 +0100

    Add files via upload

---
+++ scripts/opdracht spaties met functies.js
@@ -0,0 +1,19 @@
+const setup = () => {
+    let btnToon=document.getElementById("btnToon");
+    btnToon.addEventListener("click", update);
+};
+const update = () => {
+    let txtInput = document.getElementById("txtInput");
+    let tekst = txtInput.value;
+    let tekstMetSpaties = maakMetSpaties(tekst);
+    console.log(tekstMetSpaties);
+};
+const maakMetSpaties = (inputText) => {
+    let result = "";
+    for (let i = 0; i<inputText.length; i++) {
+        result += inputText.charAt(i);
+        result += " ";
+    }
+    return result;
+};
+window.addEventListener("load", setup);
---
+++ scripts/opdracht spaties op console.js
@@ -0,0 +1,15 @@
+const setup = () => {
+    let btnToon=document.getElementById("btnToon");
+    btnToon.addEventListener("click", update);
+};
+const update = () => {
+    let txtInput=document.getElementById("txtInput");
+    let tekst = txtInput.value;
+    let tekstMetSpaties = "";
+    for (let i = 0; i < tekst.length; i++) {
+        tekstMetSpaties += tekst.charAt(i);
+        tekstMetSpaties += " ";
+    }
+    console.log(tekstMetSpaties);
+};
+window.addEventListener("load", setup);
